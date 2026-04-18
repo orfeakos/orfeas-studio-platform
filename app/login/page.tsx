3985816a44501353c33389541012d6726e42e068
orfeakos <orfeasgv@gmail.com>
2026-04-19 01:45:07 +0300

    Fix login supabase client

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,13 @@
 'use client'
 import { useState } from 'react'
-import { supabase } from '../lib/supabase'
+import { createBrowserClient } from '@supabase/ssr'
 import { useRouter } from 'next/navigation'
 
 export default function LoginPage() {
+  const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [error, setError] = useState('')
